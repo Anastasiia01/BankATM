8c6db48693631c2259ab4cbc211816351a73cb04
Gabriel <gabrielaraujofe@gmail.com>
2020-11-05 10:59:02 -0500

    some changes

--- BankATM/BA_savings.cpp
+++ BankATM/BA_savings.cpp
@@ -1,1 +1,23 @@
 #include "BA_savings.h"
+
+BA_savings::BA_savings() {
+
+}
+
+bool BA_savings::Deposit(double checkAmount) {
+	return false;
+}
+
+
+bool BA_savings::Transfer(double amount, BusinessAccount* receiverAccount) {
+	return false;
+}
+
+
+bool BA_savings::Withdraw(double amount) {
+	return false;
+}
+
+bool BA_savings::ApplyInterest() {
+	return false;
+}
