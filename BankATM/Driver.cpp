a913ea64ed37c6caed7dac23ecbbf527f58f8486
Gabriel <gabrielaraujofe@gmail.com>
2020-11-05 12:56:41 -0500

    new driver, Certificates of deposit is missing

--- BankATM/Driver.cpp
+++ BankATM/Driver.cpp
@@ -7,13 +7,13 @@ int main()
 	Account* accounts[8];
 
 	//Hard code or randomly create the child objects
-	accounts[0] = new PersonalSavings();
-	accounts[1] = new BusinessForeignCurr();
-	accounts[2] = new BusinessSavings();
-	accounts[3] = new PersonalChecking();
-	accounts[4] = new PersonalCD();
-	accounts[5] = new BusinessHighVolChecking();
-	accounts[6] = new BusinessBusinessChecking();
+	accounts[0] = new SAPersonal();
+	accounts[1] = new FCABusiness();
+	accounts[2] = new BA_savings();
+	accounts[3] = new CAPersonal();
+	accounts[4] = new PersonalCD(); //check
+	accounts[5] = new HVCABusiness();
+	accounts[6] = new BA_checking();
 	accounts[7] = new PersonalMoneyMarket();
 
 	//display the type and id by calling a polymorphic method display
