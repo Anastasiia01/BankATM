5e2a552692df0cc7fa6eb57ca81d86ecc15d2091
Gabriel <gabrielaraujofe@gmail.com>
2020-11-05 10:57:00 -0500

    a few additions

--- BankATM/BA_checking.h
+++ BankATM/BA_checking.h
@@ -1,4 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <set>
+
+using namespace std;
+
+
 class BA_checking :public BusinessAccount {
+	private:
+		string transactionID;
+		double currentBalance;
+		double depositedAmount;
+		double withdrawAmount;
+		double transferAmount;
 
-
+	public:
+		BA_checking();
 };
