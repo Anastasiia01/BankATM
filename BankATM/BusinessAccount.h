01cff3db75e64bb3b94e827b98d3ea17e010831c
Gabriel <gabrielaraujofe@gmail.com>
2020-11-04 21:22:28 -0500

    Business Account change

--- BankATM/BusinessAccount.h
+++ BankATM/BusinessAccount.h
@@ -1,3 +1,20 @@
-class BusinessAccount {
+class BusinessAccount:public Account {
+	//test
+	public:
+
+	protected:
+
+	private:
+
+
 
 };
+/*
+ Long EIN //business ID
+String ComopanyName
+
+Savings
+Checking
+High Volume Checking
+Foreign Currency
+*/
