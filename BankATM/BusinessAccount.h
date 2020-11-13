7703920e1eb1c8db2d7943610387aa681cdf26b4
Gabriel <gabrielaraujofe@gmail.com>
2020-11-12 19:39:15 -0500

    New BusinessAccount

--- BankATM/BusinessAccount.h
+++ BankATM/BusinessAccount.h
@@ -13,19 +13,16 @@
 static int lastbusinessID = 1;
 
 class BusinessAccount:public Account {
-	//test
+	
 	private:
 		int businessID;//will be later used to display account ID in the following format A1P1S1
 		long EIN; //Employer Identification Number
 		string CompanyName;
-
-	protected://TODO: add constructor instead of set methods and call it from the child constructor
-		void setEIN(int s) { EIN = s; }
-		void setCompanyName(string nam) { CompanyName = nam; };
-		
+		long getEIN() { return EIN; }
+	
 
 	public:
-		//long getEIN() { return EIN; } it's confidential info, you don't want to make it accessible to outside world
+		BusinessAccount(string company = "University of Bridgeport");
 		string getCompanyName() { return CompanyName; }
 };
 #endif
