ec8227fa62c4cae5dda296197339299df8966ae4
Gabriel <gabrielaraujofe@gmail.com>
2020-11-13 20:53:37 -0500

    error fix

--- BankATM/BA_checking.h
+++ BankATM/BA_checking.h
@@ -15,13 +15,7 @@ using namespace std;
 
 
 class BA_checking :public BusinessAccount {
-	private:
-		//string transactionID;
-		//double currentBalance;
-		double depositedAmount;
-		double withdrawAmount;
-		double transferAmount;
-
+	
 	public:
 		BA_checking(string company = "University of Bridgeport");
 		bool Deposit(double checkAmount); //deposit check
